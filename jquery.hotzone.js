e1eee09f82653dcd2267cd83143483dead003b87
Ryan LaBouve <imryanlabouve@gmail.com>
2012-05-14 22:39:33 -0500

    Initial Commit

---
+++ jquery.hotzone.js
@@ -0,0 +1,10 @@
+/**
+* jQuery Hotzone - v0.1 5/14/2012
+* 
+* jQuery plugin for defining spacial zones of importance,
+* attaching events, and providing callbacks
+*
+* Copyright (c) 2012 Ryan LaBouve (@ryanlabouve)
+* Dual licensed under the MIT and GPL licenses.
+*
+*/
---
+++ readme.md
@@ -0,0 +1,10 @@
+#jQuery Hotzone
+
+jQuery plugin for defining spacial zones of importance, attaching events, and providing callbacks.
+
+Brought to you by [Ryan LaBouve](http://ryanlabouve.com)
+
+## Project Dependencies
+
+- jQuery (1.7.2)
+- jQuery UI (1.8.18)
